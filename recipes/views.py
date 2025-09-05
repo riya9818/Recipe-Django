6a86a9d14bd73b785cd6b8ad00a23c4ec52b91db
riya9818 <reeyabasnet999@gmail.com>
2025-09-05 14:32:49 +0545

    RecipeListing and Templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Recipe
 # Create your views here.
+def RecipeListView(request):
+    recipes = Recipe.objects.all()
+    return render(request,'../templates/recipe_list.html',{'recipes':recipes})
