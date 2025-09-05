c56b8ee54257723798f49a8025c3436f55d05c0d
riya9818 <reeyabasnet999@gmail.com>
2025-09-05 13:52:14 +0545

    Models defined

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
 # Create your models here.
+class Recipe(models.Model):
+    title= models.CharField(max_length=256)
+    ingredients= models.TextField()
+    instructions= models.TextField()
+    published_at= models.DateTimeField(auto_now_add=True)
+    author=models.ForeignKey(User , on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
