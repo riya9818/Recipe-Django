b76bc7d9c78eb432af66a22da1b600983b9ad5b2
riya9818 <reeyabasnet999@gmail.com>
2025-09-06 15:34:34 +0545

    Defined urls and settings.py

--- recipe_project/urls.py
+++ recipe_project/urls.py
@@ -17,7 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('recipes.urls')),
+    path("accounts/login/",auth_views.LoginView.as_view(),name="login"),
+    path("accounts/logout/",auth_views.LogoutView.as_view(),name="logout")
 ]
